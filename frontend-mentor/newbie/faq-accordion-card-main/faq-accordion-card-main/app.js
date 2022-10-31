74655664ba111f29ad2eace07b307d481dea9548
funupulu <t27molina@yahoo.com>
2022-10-31 09:24:47 -0700

    update to FAQ accordion
    also fixed home page by removing gtr.

--- frontend-mentor/newbie/faq-accordion-card-main/faq-accordion-card-main/app.js
+++ frontend-mentor/newbie/faq-accordion-card-main/faq-accordion-card-main/app.js
@@ -1,15 +1,31 @@
 const body = document.body
-const arrow_target = document.querySelector('.arrow')
-const answer = document.querySelector('.answer')
-const question = document.querySelector('.question')
+const arrow_target = document.querySelectorAll('.arrow')
+const answer = document.querySelectorAll('.answer')
+const question = document.querySelectorAll('.question')
 
-arrow_target.addEventListener('click', () => {
-  // console.log('test');
+arrow_target.forEach(arrow => arrow.addEventListener('click', arrowDropDown ) )
+
+function arrowDropDown () {
   if(answer.classList.contains('hide')) {
-    answer.classList.remove('hide')
-    question.style.fontWeight = '900'
-  } else {
-  answer.classList.add('hide')
-  question.style.fontWeight = 'normal'
-  }
-})
+      answer.classList.remove('hide')
+      question.style.fontWeight = '700'
+      } else {
+      answer.classList.add('hide')
+      question.style.fontWeight = 'normal'
+      }
+}
+
+// arrow_target.forEach('click', () => {
+//   console.log('test');
+// })
+
+// arrow_target.addEventListener('click', () => {
+//   // console.log('test');
+//   if(answer.classList.contains('hide')) {
+//     answer.classList.remove('hide')
+//     question.style.fontWeight = '700'
+//   } else {
+//   answer.classList.add('hide')
+//   question.style.fontWeight = 'normal'
+//   }
+// })
