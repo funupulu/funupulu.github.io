dac489c22739320db8b1a06b922afdf73c8d29e0
funupulu <t27molina@yahoo.com>
2022-10-31 16:28:54 -0700

    update to accordion file

--- frontend-mentor/newbie/faq-accordion-card-main/faq-accordion-card-main/app.js
+++ frontend-mentor/newbie/faq-accordion-card-main/faq-accordion-card-main/app.js
@@ -1,19 +1,28 @@
 const body = document.body
-const arrow_target = document.querySelectorAll('.arrow')
-const answer = document.querySelectorAll('.answer')
-const question = document.querySelectorAll('.question')
+const arrow_target_one = document.querySelector('.arrow-1')
+const arrow_target_two = document.querySelector('.arrow-2')
+const answer_one = document.querySelector('.answer-1')
+const answer_two = document.querySelector('.answer-2')
+const questionOne = document.querySelector('.question-1')
+const questionTwo = document.querySelector('.question-2')
 
-arrow_target.forEach(arrow => arrow.addEventListener('click', arrowDropDown ) )
-
-function arrowDropDown () {
-  if(answer.classList.contains('hide')) {
-      answer.classList.remove('hide')
-      question.style.fontWeight = '700'
-      } else {
-      answer.classList.add('hide')
-      question.style.fontWeight = 'normal'
-      }
+arrow_target_one.addEventListener('click', () => {
+  if(answer_one.classList.contains('hide')) {
+    answer_one.classList.remove('hide')
+  } else {
+    answer_one.classList.add('hide')
+  }
 }
+)
+
+arrow_target_two.addEventListener('click', () => {
+  if(answer_two.classList.contains('hide')) {
+    answer_two.classList.remove('hide')
+  } else {
+    answer_two.classList.add('hide')
+  }
+}
+)
 
 // arrow_target.forEach('click', () => {
 //   console.log('test');
@@ -28,4 +37,4 @@ function arrowDropDown () {
 //   answer.classList.add('hide')
 //   question.style.fontWeight = 'normal'
 //   }
-// })
+// 
